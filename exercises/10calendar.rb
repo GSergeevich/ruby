33fc63a8d3a61746678b5a7d9dd0bf0216b66278
GSergeevich <germansergeevich1@yandex.ru>
2020-10-19 16:19:06 +0300

    Add some comments in 10calendar.rb

--- exercises/10calendar.rb
+++ exercises/10calendar.rb
@@ -1,16 +1,5 @@
-
 def days_to_seconds(number)
   number * 24 * 60 * 60
-end
-
-def days_in_month(date)
-  now = Time.now
-  i = date.mday
-  tdate = Time.at(now.to_i + days_to_seconds(i))
-  while tdate.month == now.month
-   tdate = Time.at(tdate.to_i + days_to_seconds(i))
-  end
-  date.mday + i
 end
 
 def last_day_of_month(date)
@@ -21,31 +10,31 @@ def last_day_of_month(date)
   elsif Time.at(date.to_i + days_to_seconds(29 - date.mday)).month == date.month
     29
   else
-    28  
+    28
   end
 end
 
 def translate(day)
-  days_rus = {Sun: 'Вс',Mon: 'Пн',Tue: 'Вт',Wed: 'Ср',Thu: 'Чт',Fri: 'Пт',Sat: 'Сб'}
+  days_rus = { Sun: 'Вс', Mon: 'Пн', Tue: 'Вт', Wed: 'Ср', Thu: 'Чт', Fri: 'Пт', Sat: 'Сб' }
   days_rus[day]
 end
 
 year = %w[Январь Февраль Март Апрель Май Июнь Июль Август Сентябрь Октябрь Ноябрь Декабрь]
 days = %w[Sun Mon Tue Wed Thu Fri Sat]
-days_hash = {Sun: [],Mon: [],Tue: [],Wed: [],Thu: [],Fri: [],Sat: []}
+days_hash = { Sun: [], Mon: [], Tue: [], Wed: [], Thu: [], Fri: [], Sat: [] }
 
 now = Time.now
-date = Time.at(now.to_i - days_to_seconds(now.mday - 1)) #first day of month 
-p last_day = last_day_of_month(now)
+date = Time.at(now.to_i - days_to_seconds(now.mday - 1)) # first day of month
+last_day = last_day_of_month(now) # last day of month
 
-puts format('%s  %s',year[now.month - 1],now.year)
+puts format('  %<month>s  %<year>s', month: year[now.month - 1],year: now.year)
 
-week_end = 0   
-begin 
-  day = date.strftime("%a").to_sym
-  days_hash[day][0] = translate(day)
-  days_hash[day][1] = ' ' if week_end == 1
-  week_end = 1 if date.wday == 6
+first_week = 1 # is a first week in a month ? (flag)
+loop do
+  day = date.strftime('%a').to_sym # key for days_hash
+  days_hash[day][0] = translate(day) # place day name(translated)
+  days_hash[day][1] = ' ' if first_week == 0 # place space if curent-day array start not from first week of the month 
+  first_week = 0 if date.wday == 6 # first week ended
 
   i = date.mday
   while i <= last_day
@@ -54,13 +43,13 @@ begin
   end
 
   date = Time.at(date.to_i + days_to_seconds(1))
-
-end while date.mday <= 7
+  break unless date.mday <= 7
+end
 
 days.each do |day|
   day = day.to_sym
   days_hash[day].each do |day|
-    print format('%2s ',day)
+    print format('%2s ', day)
   end
   print "\n"
 end
